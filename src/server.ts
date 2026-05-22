25b63124f1727e2ab8d86bb03d1dc5a5fa0f2844
Angel <sevinchoyazimboevna@gmail.com>
2026-05-22 06:18:32 +0500

    FEAT: INITIAL SETUP

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
-console.log("Executed");  
+// console.log("Executed");  
 
-import moment from 'moment';
+// import moment from 'moment';
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
 
-const person: string = "Angel";
-const count: number = 100;
+// const person: string = "Angel";
+// const count: number = 100;
+
+//ARCHITECTUAL PATTERN
+//1 MVC - MODEL VIEW CONTROLLER
+// 2 DEPENDENCY INJECTION
+// 3 MVP
+
+//DESIGN PATTERN
+// 1 MIDDLEWARE,
+// 2 DECOTAR
+
+// PATTERN - MEANS NAQSH
