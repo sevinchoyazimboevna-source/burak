bdf7d7eb3a3658960af41fbef0c10b0d050bf66c
Angel <sevinchoyazimboevna@gmail.com>
2026-05-22 05:53:28 +0500

    FEAT: INITIAL SETUP

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("Executed");  
+
+import moment from 'moment';
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Angel";
+const count: number = 100;
