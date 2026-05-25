eb902a38ccbfca8c31d9efc298ede2d25de7c8ff
Angel <sevinchoyazimboevna@gmail.com>
2026-05-26 04:27:12 +0500

    FEAT: MITASK-M TYPESCRIPT

--- src/trainn.ts
+++ src/trainn.ts
@@ -1,1 +1,20 @@
 console.log("Hello World");
+
+console.log("MITASK-M");
+
+function getSquareNumbers(arr:number[]) {
+    let result: {
+        number: number;
+        square: number
+    }[] = []
+    for (let num of arr) { //array ichidaki har bir sonni aladi
+      let obj = {
+        number: num,
+        square: num ** 2
+      }
+      result.push(obj);
+    }
+    return result
+}
+
+console.log(getSquareNumbers([1,2,4,5,7,8,9])); //good
