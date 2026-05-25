cf54fa2d3145c6a9b75c90f8463216f3753cba78
Angel <sevinchoyazimboevna@gmail.com>
2026-05-26 04:15:05 +0500

    FEAT: MITASK-M

--- train.py
+++ train.py
@@ -1,17 +1,35 @@
-# MITASK-L
 
-print("===========START==============")
+# print("=========== MITASK-L==============")
 
-def obratnoText(text):
-    words = text.split()   # split  gapni sozlarga bolib oladi
-    result = []
+# def obratnoText(text):
+#     words = text.split()   # split  gapni sozlarga bolib oladi
+#     result = []
 
-    for word in words:
-        result.append(word[::-1])   # sozni teskari qiladi
+#     for word in words:
+#         result.append(word[::-1])   # sozni teskari qiladi
 
-    return " ".join(result)
+#     return " ".join(result)
 
 
-print(obratnoText("Oi eu sou a Angel!"))
+# print(obratnoText("Oi eu sou a Angel!"))
 
-print("===========DONE==============")
+
+
+print("===========MITASK-M==============")
+
+def getSquareNumbers(arr):
+    result = [] #natijalarni saqlash uchun bosh list
+    for num in arr: 
+
+        obj = {
+            "number": num,
+            "square": num **2 #bu yerda 2 emas hohlagan number qoyib qiymat chikarsak boladi
+        }
+
+        result.append(obj)
+
+    return result
+
+print(getSquareNumbers([1,2,5,7,4,9,0]))
+
+
