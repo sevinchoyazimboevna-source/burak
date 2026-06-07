373e052f20fa5b9291695be2f2a414d27d75fc3d
Angel <sevinchoyazimboevna@gmail.com>
2026-06-07 14:57:24 +0500

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,16 @@ import routerAdmin from "./routerAdmin";
 import morgan from 'morgan';
 import { MORGAN_FORMAT } from './libs/config';
 
+
+import session from "express-session";
+import ConnectMongoDB from 'connect-mongodb-session';
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL),
+    collection: "session",
+});
+
 // 4ta bolim
 
 /*1-ENTRANCE */
@@ -18,6 +28,19 @@ app.use(morgan(MORGAN_FORMAT));
 
 
 /*2-SESSION */
+app.use(
+    session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+    maxAge: 1000 * 60 * 60 * 3,  // 1 week
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true
+    })
+);
+
+
 /*3-VIEWS */
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -28,6 +51,8 @@ app.use("/admin", routerAdmin);  //SSR: EJS
 app.use('/', router); //Design pattern middleware  SPA: REACT
 
 export default app; //app ishga tushishi uchun export qilishimiz kerak
+
+
 
 
 // 1
