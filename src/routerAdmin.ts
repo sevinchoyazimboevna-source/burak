4903c7c6a2d47fb70bfd660e32cbe23ff256f516
Angel <sevinchoyazimboevna@gmail.com>
2026-06-11 07:38:22 +0500

    feat: last process

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,6 +2,7 @@ import express from 'express';
 const routerAdmin = express.Router();
 import restaurantController from './controllers/restaurant.controller';
 import productController from './controllers/product.controller';
+import makeUploader from './libs/utils/uploader';
 
 routerAdmin.get('/', restaurantController.goHome);
 
@@ -12,7 +13,9 @@ routerAdmin
 
 routerAdmin
 .get('/signup', restaurantController.getSignup)
-.post('/signup', restaurantController.processSignup);
+.post('/signup',
+makeUploader("members").single("memberImage"),    
+restaurantController.processSignup);
 
 routerAdmin
 .get('/logout', restaurantController.logout)
@@ -27,6 +30,8 @@ productController.getAllProducts);
 
 routerAdmin.post('/product/create',
 restaurantController.verifyRestaurant, 
+// uploadProductImage.single("productImage"),
+makeUploader("products").array("productImages", 5),
 productController.createNewProduct);
 
 routerAdmin.post('/product/:id',
