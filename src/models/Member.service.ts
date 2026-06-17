04110fdf5eb51cec6583249227c4873a0bc7dc3b
Angel <sevinchoyazimboevna@gmail.com>
2026-06-17 06:08:26 +0500

    feat: develop updatechosenuser api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,8 +1,9 @@
 import MemberModel from "../schema/Member.model";
-import {  LoginInput, Member, MemberInput } from "../libs/types/member";
+import {  LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors, {HttpCode, Message} from "../libs/Errors";
 import {MemberType} from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
+import { shapeIntMongooseObjectId } from "../libs/config";
 
 // bcrypt — bu parollarni xavfsiz saqlash uchun ishlatiladigan hashlash algoritmi.
 
@@ -110,6 +111,16 @@ class MemberService {
    .find({MemberType: MemberType.USER})
    .exec();
    if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+   return result;
+  }
+
+  public async updateChosenUser(input: MemberUpdateInput): Promise<Member> {
+   input._id = shapeIntMongooseObjectId(input._id);
+   const result = await this.memberModel
+   .findByIdAndUpdate({_id: input._id}, input, {new: true})
+   .exec();
+   if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
 
    return result;
   }
--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -39,8 +39,13 @@ productController.updateChosenProduct); //:id: param
 
 
 //user
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant,
+routerAdmin.get("/user/all", 
+restaurantController.verifyRestaurant,
 restaurantController.getUsers);
+
+routerAdmin.post("/user/edit", 
+restaurantController.verifyRestaurant,
+restaurantController.updateChosenUser);
 
 
 export default routerAdmin;
