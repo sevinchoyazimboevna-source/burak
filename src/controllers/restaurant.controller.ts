da4b8ec2d26fd70bd235bdf5602bcb97c67bc785
Angel <sevinchoyazimboevna@gmail.com>
2026-06-19 05:24:10 +0500

    feat: signup process fronted validation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -45,7 +45,8 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
-    console.log("processSignup")
+    console.log("processSignup");
+    console.log("reqbody", req.body);
     const file = req.file;
     if(!file)
       throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
