18662a5243e7b971373466e6e75b9e717848f387
Angel <sevinchoyazimboevna@gmail.com>
2026-06-23 04:20:52 +0500

    fix: modify spa login rest api logic process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,7 @@
 import MemberModel from "../schema/Member.model";
 import {  LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors, {HttpCode, Message} from "../libs/Errors";
-import {MemberType} from "../libs/enums/member.enum";
+import {MemberStatus, MemberType} from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntMongooseObjectId } from "../libs/config";
 
@@ -36,19 +36,24 @@ class MemberService {
   public async login(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne(
-        { memberNick: input.memberNick },
-        { memberNick: 1, memberPassword: 1 }
+        { memberNick: input.memberNick, memberStatus: {
+          $ne: MemberStatus.DELETE
+        }, },
+        { memberNick: 1, memberPassword: 1, memberStatus: 1}
       )
       .exec();
 
-    if (!member) {
+    if (!member)
       throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    else if(member.memberStatus === MemberStatus.BLOCK) {
+      throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
     }
 
     const isMatch = await bcrypt.compare(
       input.memberPassword,
       member.memberPassword
-    );
+    );  
 
     if (!isMatch) {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
